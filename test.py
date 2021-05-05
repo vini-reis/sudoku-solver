13e1261231b805f1c6a85248cc509d392cdc8ecd
Vinícius Reis <vinecreis@gmail.com>
2021-05-04 23:33:23 -0300

    Verifying some CSP rules

--- test.py
+++ test.py
@@ -1,14 +1,4 @@
 import sudoku as s
 
-b = s.Board(".......2143.......6........2.15..........637...........68...4.....23........7....")
-print("Rows:")
-for i in range(9):
-    print(b.getRow(i))
-
-print("Cols:")
-for i in range(9):
-    print(b.getColumn(i))
-
-print("Boxes:")
-for i in range(9):
-    print(b.getBox(i))
+c = s.CSP(".......2143.......6........2.15..........637...........68...4.....23........7....")
+print(s.backtracking(c))

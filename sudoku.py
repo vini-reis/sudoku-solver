3045b45dd67e04439a0b9e31b2b60a924927dd46
Vinícius Reis <vinecreis@gmail.com>
2021-05-22 08:37:13 -0300

    Trying new aproacjes for solving Sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,6 @@
-from ac3 import neighboors
 import numpy as np
 import math
+import progressbar
 
 possibilities = range(1,10)
 
@@ -12,17 +12,10 @@ class Board():
         self._cols = np.array([[self._rows[i][j] for i in range(9)] for j in range(9)])
         self._boxs = np.array([[self._rows[i+3*l][j+3*k] for i in range(3) for j in range(3)] for l in range(3) for k in range(3)])
 
+        self.neighboors : list = [set([math.floor(i / 9) * 9 + k for k in range(9)] + [(i % 9) + 9 * k for k in range(9)] + [(j + 3 * math.floor((i % 9) / 3)) + (math.floor(i / 27) * 27 + k * 9) for k in range(3) for j in range(3)]) for i in range(81)]
+
     def __str__(self):
         return "".join(self._flat)
-
-    def getRow(self, row : int) -> list or None:
-        return self._rows[row] if row >= 0 and row < 9 else None
-
-    def getColumn(self, col : int) -> list or None:
-        return self._cols[col] if col >= 0 and col < 9 else None
-    
-    def getBox(self, box : int) -> list or None:
-        return self._boxs[box] if box >= 0 and box < 9 else None
 
     def check(self) -> bool:
         for r in self._rows:
@@ -68,11 +61,12 @@ class Board():
 class CSP(Board):
     def __init__(self, state: str):
         super().__init__(state)
-        self.neighboors : list = [set([math.floor(i / 9) * 9 + k for k in range(9)] + [(i % 9) + 9 * k for k in range(9)] + [(j + 3 * math.floor((i % 9) / 3)) + (math.floor(i / 27) * 27 + k * 9) for k in range(3) for j in range(3)]) for i in range(81)]
 
         self.X : list = [i for i in range(81) if self._flat[i] == 0]
         self.C : list = [set([self._flat[e] for e in self.neighboors[c] if e not in self.X]) if c in self.X else [p for p in possibilities if p != self._flat[c]] for c in range(81)]
         self.D : list = [[v for v in possibilities if v not in self.C[c]] if c in self.X else [self._flat[c]] for c in range(81)]
+        self.X = [x for x in self.X if len(self.D[x]) > 1]
+        self._flat = np.array([self.D[c][0] if len(self.D[c]) == 1 else 0 for c in range(81)])
 
     def __str__(self):
         return "".join([str(self.D[c][0]) if len(self.D[c]) == 1 else '.' for c in range(81)])
@@ -87,14 +81,19 @@ class CSP(Board):
         for c in range(81):
             if len(self.D[c]) != 1:
                 return False
-        return self.check()
+            elif self.D[c][0] in self.C[c]:
+                return False
+        return True
 
     def update(self, i : int or None = None):
         # FIXME: Check corretude of updated CSP
-        rng = self.neighboors[i] if i is not None else range(81)
-        self.C = [set([v for e in self.neighboors[c] for v in self.D[e] if e not in self.X]) if c in self.X and c in rng else self.C[c] for c in range(81)]
-        self.D = [[v for v in self.D[c] if v not in self.C[c]] if c in self.X and c in rng else self.D[c] for c in range(81)]
-        self.X = [x for x in self.X if len(self.D[x]) != 1]
+        # rng = self.neighboors[i] if i is not None else range(81)
+        rng = range(81)
+        if i is not None and i in self.X: self.X.remove(i);
+        self.C = [set([self.D[e][0] for e in self.neighboors[c] if e not in self.X]) if c in self.X else set([p for p in possibilities if p not in self.D[c]]) for c in range(81)]
+        self.D = [[v for v in self.D[c] if v not in self.C[c]] if c in self.X else self.D[c] for c in range(81)]
+        self.X = [x for x in self.X if len(self.D[x]) > 1]
+        self._flat = np.array([self.D[c][0] if len(self.D[c]) == 1 else 0 for c in range(81)])
 
     def test(self, i : int, vi : int, vj : int):
         test_set : set = {vi}
@@ -118,10 +117,9 @@ def AC3(csp : CSP, queue : list):
 
     i,j = queue.pop()
     if revise(csp, i, j):
-        if len(csp.D[i]) == 0: return False
+        if not csp.valid(): return False
         [queue.append((k,i)) for k in csp.neighboors[i] if k != i and (k,i) not in queue]
         csp.update(i)
-        # print("Revised!")
     return AC3(csp, queue)
 
 def selectVar(csp : CSP) -> int:
@@ -129,15 +127,19 @@ def selectVar(csp : CSP) -> int:
     return sorted(count, reverse=True)[0][1]
 
 def sortValues(i : int, csp : CSP):
-    count = [(len([True for k in csp.X if v in csp.D[k]]), v) for v in csp.D[i]]
-    count = sorted(count)
+    count = [(len([True for k in csp.neighboors[i] if v in csp.D[k] and k in csp.X]), v) for v in csp.D[i]]
+    count = sorted(count, reverse=True)
     return [v[1] for v in count]
 
-def assign(v, i, csp) -> CSP:
-    csp2 : CSP = CSP(str(csp))
-    csp2.D[i] = [v]
-    csp2.update(i)
-    return AC3(csp2, csp2.queue()) if csp2.check() else False
+def assign(v, i, csp : CSP):
+    csp_novo : CSP = CSP(str(csp))
+    csp_novo.D[i] = [v]
+    if csp_novo.valid() and csp_novo.check():
+        csp_novo.update(i)
+        csp = csp_novo
+        return True, csp
+        return AC3(csp, csp.queue())
+    return False, csp
 
 def backtracking(csp : CSP):
     print(csp, csp.check())
@@ -145,8 +147,20 @@ def backtracking(csp : CSP):
     if not csp.valid(): return False
     i = selectVar(csp)
     for v in csp.D[i]:
-        sol : CSP or bool = assign(v,i,csp)
-        if sol:
+        sol : CSP or bool = assign(v,i,CSP(str(csp)))
+        if sol and sol.valid() and sol.check():
             sol = backtracking(sol)
+            if sol: return sol
+    return False
+
+def pureBack(csp : CSP):
+    print(csp)
+    if len(csp.X) == 0: return csp
+
+    i = selectVar(csp)
+    for v in csp.D[i]:
+        consistent, csp = assign(v, i, csp)
+        if consistent:
+            sol = pureBack(csp)
             if sol: return sol
     return False
